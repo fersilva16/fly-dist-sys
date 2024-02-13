f89b0669f5e8f4975114cd3f41c2380e1e2effd4
Fernando Silva <fernandonsilva16@gmail.com>
2024-02-13 18:22:05 -0300

    feat(broadcast): add challenge

--- main.go
+++ main.go
@@ -41,7 +41,45 @@ func main() {
 		return n.Reply(msg, body);
 	})
 
+	var messages []any;
+
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		var body map[string]any;
+
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err;
+		}
+
+		messages = append(messages, body["message"])
+
+		res_body := make(map[string]any);
+
+		res_body["type"] = "broadcast_ok";
+		
+		return n.Reply(msg, res_body);
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		var body map[string]any;
+
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err;
+		}
+
+		body["type"] = "read_ok";
+		body["messages"] = messages;
+
+		return n.Reply(msg, body);
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		body := make(map[string]any)
+
+		body["type"] = "topology_ok";
+
+		return n.Reply(msg, body);
+	})
 	if err := n.Run(); err != nil {
-		log.Fatal(err)
+		log.Fatal(err);
 	}
 }
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,4 @@
 go build -o build
 # maelstrom test -w echo --bin ./build/fly-dist-sys --node-count 1 --time-limit 10
-maelstrom test -w unique-ids --bin ./build/fly-dist-sys --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
+# maelstrom test -w unique-ids --bin ./build/fly-dist-sys --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
+maelstrom test -w broadcast --bin ./build/fly-dist-sys --node-count 1 --time-limit 20 --rate 10
