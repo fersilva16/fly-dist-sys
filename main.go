d42cdfcf4b81a985faf32c907187fcad131dea77
Fernando Silva <fernandonsilva16@gmail.com>
2024-02-13 18:09:54 -0300

    feat(unique-ids): add challenge

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"strconv"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -14,12 +16,29 @@ func main() {
 		var body map[string]any;
 
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
+			return err;
 		}
 
 		body["type"] = "echo_ok";
 
 		return n.Reply(msg, body)
+	})
+
+	var count int64;
+
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		var body map[string]any;
+
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err;
+		}
+			
+		body["type"] = "generate_ok";
+		body["id"] = n.ID() + strconv.FormatInt(time.Now().Unix(), 10) + strconv.FormatInt(count, 10);
+
+		count += 1;
+		
+		return n.Reply(msg, body);
 	})
 
 	if err := n.Run(); err != nil {
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,3 @@
 go build -o build
-maelstrom test -w echo --bin ./build/fly-dist-sys --node-count 1 --time-limit 10
+# maelstrom test -w echo --bin ./build/fly-dist-sys --node-count 1 --time-limit 10
+maelstrom test -w unique-ids --bin ./build/fly-dist-sys --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
