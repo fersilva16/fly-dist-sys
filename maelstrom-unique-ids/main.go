eb05cfd1c5baf40b98f9f1561d4de60eb173d1c3
Fernando Silva <fernandonsilva16@gmail.com>
2024-02-18 13:50:53 -0300

    feat(json): add structs (#9)

--- maelstrom-unique-ids/main.go
+++ maelstrom-unique-ids/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"strconv"
 	"time"
@@ -14,14 +13,8 @@ func main() {
 
   var count int64;
 
-  node.Handle("generate", func(msg maelstrom.Message) error {
-    var body map[string]any;
-
-    if err := json.Unmarshal(msg.Body, &body); err != nil {
-      return err;
-    }
-  
-     id := node.ID() + strconv.FormatInt(time.Now().Unix(), 10) + strconv.FormatInt(count, 10);
+  node.Handle("generate", func(msg maelstrom.Message) error {  
+		id := node.ID() + strconv.FormatInt(time.Now().Unix(), 10) + strconv.FormatInt(count, 10);
 
     count += 1;
 
