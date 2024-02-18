d296fe4ec99b66a23c0a04cb505ad10263f8e3c5
Fernando Silva <fernandonsilva16@gmail.com>
2024-02-18 13:29:27 -0300

    fix(whitespace): fix

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -10,92 +10,92 @@ import (
 )
 
 func main() {
-	node := maelstrom.NewNode()
+  node := maelstrom.NewNode()
 
-	var messages []any;
-	neighbours := map[string]*sync.Map{};
+  var messages []any;
+  neighbours := map[string]*sync.Map{};
 
-	node.Handle("broadcast", func(msg maelstrom.Message) error {
-		var body map[string]any;
+  node.Handle("broadcast", func(msg maelstrom.Message) error {
+    var body map[string]any;
 
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err;
-		}
+    if err := json.Unmarshal(msg.Body, &body); err != nil {
+      return err;
+    }
 
-		if (slices.Contains(messages, body["message"])) {
-			return nil;
-		}
+    if (slices.Contains(messages, body["message"])) {
+      return nil;
+    }
 
-		messages = append(messages, body["message"]);
-		
-		for id, messages := range neighbours {			
-			neighbour_message := map[string]any{
-				"type": "broadcast",
-				"message": body["message"],
-			}
-			
-			messages.Store(body["msg_id"], neighbour_message);
+    messages = append(messages, body["message"]);
+    
+    for id, messages := range neighbours {      
+      neighbour_message := map[string]any{
+        "type": "broadcast",
+        "message": body["message"],
+      }
+      
+      messages.Store(body["msg_id"], neighbour_message);
 
-			go func(messages *sync.Map, id string) {
-				messages.Range(func (key any, raw_value any) bool {
-					message := raw_value.(map[string]any);
-					
-					node.RPC(id, message, func (reply_msg maelstrom.Message) error {
-						messages.Delete(key);
-						
-						return nil;
-					});
-					
-					return true;
-				});
-			}(messages, id);
-		}
+      go func(messages *sync.Map, id string) {
+        messages.Range(func (key any, raw_value any) bool {
+          message := raw_value.(map[string]any);
+          
+          node.RPC(id, message, func (reply_msg maelstrom.Message) error {
+            messages.Delete(key);
+            
+            return nil;
+          });
+          
+          return true;
+        });
+      }(messages, id);
+    }
 
-		res_body := map[string]any{
-			"type": "broadcast_ok",
-		};
-		
-		return node.Reply(msg, res_body);
-	})
+    res_body := map[string]any{
+      "type": "broadcast_ok",
+    };
+    
+    return node.Reply(msg, res_body);
+  })
 
-	node.Handle("read", func(msg maelstrom.Message) error {
-		var body map[string]any;
+  node.Handle("read", func(msg maelstrom.Message) error {
+    var body map[string]any;
 
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err;
-		}
+    if err := json.Unmarshal(msg.Body, &body); err != nil {
+      return err;
+    }
 
-		res_body := map[string]any{
-			"type": "read_ok",
-			"messages": messages,
-		}
+    res_body := map[string]any{
+      "type": "read_ok",
+      "messages": messages,
+    }
 
-		return node.Reply(msg, res_body);
-	})
-	
-	node.Handle("topology", func(msg maelstrom.Message) error {
-		var body map[string]any;
+    return node.Reply(msg, res_body);
+  })
+  
+  node.Handle("topology", func(msg maelstrom.Message) error {
+    var body map[string]any;
 
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err;
-		}
+    if err := json.Unmarshal(msg.Body, &body); err != nil {
+      return err;
+    }
 
-		topology := body["topology"].(map[string]interface {})[node.ID()].([]interface{});
+    topology := body["topology"].(map[string]interface {})[node.ID()].([]interface{});
 
-		for i := 0; i < len(topology); i++ {
-			id := topology[i].(string)
+    for i := 0; i < len(topology); i++ {
+      id := topology[i].(string)
 
-			neighbours[id] = &sync.Map{};
-		}
+      neighbours[id] = &sync.Map{};
+    }
 
-		res_body := map[string]any{
-			"type": "topology_ok",
-		}
+    res_body := map[string]any{
+      "type": "topology_ok",
+    }
 
-		return node.Reply(msg, res_body);
-	})
-	
-	if err := node.Run(); err != nil {
-		log.Fatal(err);
-	}
+    return node.Reply(msg, res_body);
+  })
+  
+  if err := node.Run(); err != nil {
+    log.Fatal(err);
+  }
 }
--- maelstrom-unique-ids/main.go
+++ maelstrom-unique-ids/main.go
@@ -10,30 +10,30 @@ import (
 )
 
 func main() {
-	node := maelstrom.NewNode()
+  node := maelstrom.NewNode()
 
-	var count int64;
+  var count int64;
 
-	node.Handle("generate", func(msg maelstrom.Message) error {
-		var body map[string]any;
+  node.Handle("generate", func(msg maelstrom.Message) error {
+    var body map[string]any;
 
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err;
-		}
-	
- 		id := node.ID() + strconv.FormatInt(time.Now().Unix(), 10) + strconv.FormatInt(count, 10);
+    if err := json.Unmarshal(msg.Body, &body); err != nil {
+      return err;
+    }
+  
+     id := node.ID() + strconv.FormatInt(time.Now().Unix(), 10) + strconv.FormatInt(count, 10);
 
-		count += 1;
+    count += 1;
 
-		res_body := map[string]any{
-			"type": "generate_ok",
-			"id": id,
-		};
-		
-		return node.Reply(msg, res_body);
-	})
-	
-	if err := node.Run(); err != nil {
-		log.Fatal(err);
-	}
+    res_body := map[string]any{
+      "type": "generate_ok",
+      "id": id,
+    };
+    
+    return node.Reply(msg, res_body);
+  })
+  
+  if err := node.Run(); err != nil {
+    log.Fatal(err);
+  }
 }
