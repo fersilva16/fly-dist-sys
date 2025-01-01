f5ff011a77cd835c704de68537b2e2a7eff64739
Fernando Silva <fernandonsilva16@gmail.com>
2025-01-01 00:47:52 -0300

    feat(flake): update (#22)

--- flake.nix
+++ flake.nix
@@ -9,44 +9,21 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlay = self: super: with self; {
-          jepsen-maelstrom = stdenv.mkDerivation {
-            name = "maelstrom";
-            version = "0.2.3";
-            src = builtins.fetchurl {
-              url = "https://github.com/jepsen-io/maelstrom/releases/download/v0.2.3/maelstrom.tar.bz2";
-              sha256 = "sha256:06jnr113nbnyl9yjrgxmxdjq6jifsjdjrwg0ymrx5pxmcsmbc911";
-            };
-            installPhase = ''
-              mkdir -p $out/bin
-              cp -r lib $out/bin/lib
-              install -m755 -D maelstrom $out/bin/maelstrom
-            '';
-          };
-
-          dist-sys-shell = stdenv.mkDerivation {
-            name = "dist-sys-shell";
-            nativeBuildInputs = [
-              go
-              openjdk
-              graphviz
-              gnuplot
-              jepsen-maelstrom
-            ];
-          };
-        };
-
         pkgs = import nixpkgs {
           inherit system;
+          
           config.allowUnfree = true;
-
-          overlays = [
-            overlay
-          ];
         };
       in
       {
-        devShell = pkgs.dist-sys-shell;
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+              go
+              graphviz
+              gnuplot
+              maelstrom-clj
+          ];
+        };
       }
     );
 }
