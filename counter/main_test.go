e3e0b07ae81a0bd6396d0aaac792b1da913a38f5
Fernando Silva <fernandonsilva16@gmail.com>
2025-01-09 18:35:59 -0300

    feat(counter): add retry on kv cas (#49)

--- counter/main_test.go
+++ counter/main_test.go
@@ -66,3 +66,69 @@ func Test(t *testing.T) {
 
 	snaps.MatchSnapshot(t, output)
 }
+
+func TestRetry(t *testing.T) {
+	require := require.New(t)
+
+	node = maelstrom.NewNode()
+
+	link := testutils.NewLink(node)
+	client := testutils.NewClient("c0", link)
+	kv := testutils.NewSeqKV(link)
+
+	go main()
+
+	err := client.InitNode("n0", []string{"n0"})
+
+	require.NoError(err)
+
+	err = client.Write(AddRequest{
+		MessageBody: maelstrom.MessageBody{
+			Type:  "add",
+			MsgID: 2,
+		},
+
+		Delta: 1,
+	})
+
+	require.NoError(err)
+
+	err = kv.HandleRead("counter", 0)
+
+	require.NoError(err)
+
+	err = kv.HandleCASConflict("counter", float64(0), float64(1), true)
+
+	require.NoError(err)
+
+	err = kv.HandleRead("counter", 0)
+
+	require.NoError(err)
+
+	err = kv.HandleCAS("counter", float64(0), float64(1), true)
+
+	require.NoError(err)
+
+	output, err := client.Read()
+
+	require.NoError(err)
+
+	snaps.MatchSnapshot(t, output)
+
+	err = client.Write(maelstrom.MessageBody{
+		Type:  "read",
+		MsgID: 3,
+	})
+
+	require.NoError(err)
+
+	err = kv.HandleRead("counter", 1)
+
+	require.NoError(err)
+
+	output, err = client.Read()
+
+	require.NoError(err)
+
+	snaps.MatchSnapshot(t, output)
+}
