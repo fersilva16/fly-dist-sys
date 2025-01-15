3becc20ba3889b21a7f1657383c0a59842d3bc33
Fernando Silva <fernandonsilva16@gmail.com>
2025-01-15 12:56:22 -0300

    feat(txn): add single-node challenge (#60)

--- txn/a/main.go
+++ txn/a/main.go
@@ -1,15 +1,66 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+type TxnRequest struct {
+	maelstrom.MessageBody
+
+	Txn []Op `json:"txn"`
+}
+
+type TxnResponse struct {
+	maelstrom.MessageBody
+
+	Txn []Op `json:"txn"`
+}
+
 var node = maelstrom.NewNode()
 
 func main() {
-	// TODO
+	store := NewStore()
+
+	node.Handle("txn", func(msg maelstrom.Message) error {
+		var body TxnRequest
+
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		res := []Op{}
+
+		for _, op := range body.Txn {
+			if op.fn == WRITE {
+				store.Write(op.key, op.value)
+
+				res = append(res, Op{WRITE, op.key, op.value})
+
+				continue
+			}
+
+			value, ok := store.Read(op.key)
+
+			if !ok {
+				value = 0
+			}
+
+			res = append(res, Op{READ, op.key, value})
+		}
+
+		resBody := TxnResponse{
+			MessageBody: maelstrom.MessageBody{
+				Type: "txn_ok",
+			},
+
+			Txn: res,
+		}
+
+		return node.Reply(msg, resBody)
+	})
 
 	if err := node.Run(); err != nil {
 		log.Fatal(err)
